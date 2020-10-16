77a80765e90f606303db485a081bcfa60174d400
Steven Etienne <setienne@advsol.com>
2020-10-16 08:43:52 -0500

    Initial Commit

---
+++ AsiCSharpClient/AsiCSharpClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsiCSharpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
